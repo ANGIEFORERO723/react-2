fecd38ef6c21a19a7534d9a0538dfece6821f694
Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2022-06-07 19:42:47 -0500

    [update] Changed class name and removed unnused code

--- familinsurance_react-master/src/components/Card/Card.js
+++ familinsurance_react-master/src/components/Card/Card.js
@@ -4,15 +4,11 @@ import "./Card.css";
 export default function Card({Imagen, Nombre, Descripcion }) {
   return (
     <div className="card" > 
-      {/*<img src={image} alt={title} />
-      <h1>{title}</h1>
-      <p>{description}</p*/}
-      <p className="test-color">lorem ipsum </p>
-      <p className="test-color">lorem ipsum </p>
+      <p className="red-color">lorem ipsum </p>
+      <p className="red-color">lorem ipsum </p>
       <a href="chatea-nosotros.html" class="iitem item-style">
         <div class="iitem-icon">
           <img src={Imagen} width="75" height="75" alt="PathSoft" />
-          {/*<i class="material-icons material-icons-outlined md-48">support_agent</i> */}
         </div>
         <div class="iitem-icon-bg">
           <i class="material-icons material-icons-outlined">support_agent</i>
