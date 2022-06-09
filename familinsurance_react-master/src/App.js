ce345fdd1639668bea197df5cc3ede9cf2267ec7
Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2022-06-08 19:35:40 -0500

    [update] Added form to get data from the user to the newsletter

--- familinsurance_react-master/src/App.js
+++ familinsurance_react-master/src/App.js
@@ -6,6 +6,7 @@ import Contenedores from "./components/cuerpo/contenedores";
 import Slider from './components/Slider/Slider';
 import Services from './components/Services/Services';
 import Footer from './components/Footer/Footer';
+import Form from "./components/Form/Form";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       <Cuerpo/>
       {/*<Contenedores/>*/}
       {/*<Slider />*/}
-      <Services />
+      {/* <Services /> */}
+      <Form />
       <Footer />
     </div>
   );
